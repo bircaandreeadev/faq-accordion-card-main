5cbc0be20cdda6af67012f8d131e26ee44a2dd9b
andreeabircadev <andreeabirca17@gmail.com>
2021-01-15 21:24:52 +0200

    fixed js and style

--- actions.js
+++ actions.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
-        $('li.element').click(function () {
-                $(this).find('p').toggle();
-                $(this).toggleClass('text-bold');
+        $('article.element').click(function () {
+                
+                if($(this).hasClass('show-text')) {
+                        $('article.element').removeClass('show-text');
+                        $(this).removeClass('show-text');
+                } else {
+                        $('article.element').removeClass('show-text');
+                        $(this).addClass('show-text');
+                }
+                
+                // $('article.element .question-text').hide();
+                
+
         });
 });
