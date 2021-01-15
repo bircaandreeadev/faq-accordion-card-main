4c73e2bef36a9b312c416bbf79ada260e8073829
andreeabircadev <andreeabirca17@gmail.com>
2021-01-15 21:47:58 +0200

    replaced article tag with div

--- actions.js
+++ actions.js
@@ -1,15 +1,15 @@
 $( document ).ready(function() {
-        $('article.element').click(function () {
+        $('div.element').click(function () {
                 
                 if($(this).hasClass('show-text')) {
-                        $('article.element').removeClass('show-text');
+                        $('div.element').removeClass('show-text');
                         $(this).removeClass('show-text');
                 } else {
-                        $('article.element').removeClass('show-text');
+                        $('div.element').removeClass('show-text');
                         $(this).addClass('show-text');
                 }
                 
-                // $('article.element .question-text').hide();
+                // $('div.element .question-text').hide();
                 
 
         });
